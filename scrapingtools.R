59a819c21fde5e891e1cb013ec2a4a6298ac1cee
willoutcault <57675974+willoutcault@users.noreply.github.com>
2020-05-04 16:10:00 -0400

    Update scrapingtools.R

--- scrapingtools.R
+++ scrapingtools.R
@@ -19,22 +19,31 @@ skillscrape <- function(job,city){
     urls <- list()
     
     #### Counting Loops
-    
-    
     main_page <- read_html(paste("https://www.careerbuilder.com/jobs?keywords=",search_job,"&location=",search_loc, sep = ""))
+    count <- main_page %>% 
+        html_nodes("div#job-count.col.b.dark-blue-text") %>% 
+        html_text()
+    count <- as.numeric(as.character(str_remove_all(count, "[^[:digit:]]")))
+    loop_count <- round(count/25)
+    if (loop_count > 2){
+        loop_count <- 2
+        
+    }
+    for (i in 1:loop_count){
+        try(main_page <- read_html(paste("https://www.careerbuilder.com/jobs?keywords=",search_job,"&location=",search_loc,"&page_number=",i, sep = "")))
     
     ##### URLS
-    url <- main_page %>% 
-        html_nodes("a.data-results-content.block.job-listing-item") %>% 
-        html_attr("href")
-    urls <- append(urls, url)
-    
+        url <- main_page %>% 
+            html_nodes("a.data-results-content.block.job-listing-item") %>% 
+            html_attr("href")
+        urls <- append(urls, url)
+    }
     closeAllConnections()
     
     
     #### Append Skills
     
-    for (i in 1:25){
+    for (i in 1:length(urls){
         try(job_posting <- read_html(paste("https://www.careerbuilder.com",urls[[i]],sep="")))
         try(append_df[i,1] <- job)
         try(skills <- list(job_posting %>% 
