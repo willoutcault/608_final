204deffd27bc6ee95896a3530591018cfe4d3f24
willoutcault <57675974+willoutcault@users.noreply.github.com>
2020-05-04 19:39:07 -0400

    Update scrapingtools.R

--- scrapingtools.R
+++ scrapingtools.R
@@ -35,7 +35,7 @@ skillscrape <- function(job,city){
         skills <- list(job_posting %>% 
                            html_nodes(".check-bubble") %>% 
                            html_text()))
-        try(append_df[i,2] <- list(skills)
+        append_df[i,2] <- list(skills)
     }
     closeAllConnections()
     new_df <- rbind(new_df, append_df)
