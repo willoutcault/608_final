f5a39d1ff78eb93d7577bb673457c5790e76b955
willoutcault <57675974+willoutcault@users.noreply.github.com>
2020-05-05 22:54:28 -0400

    Update app.R

--- app.R
+++ app.R
@@ -3,98 +3,83 @@ require(tidyr)
 require(stringr)
 require(rvest)
 require(shiny)
+require(leaflet)
 require(shinythemes)
 require(ggplot2)
+require(shinyWidgets)
+require(shinydashboard)
+require(DT)
 source("https://raw.githubusercontent.com/willoutcault/608_final/master/scrapingtools.R")
 coords <- read.csv('https://raw.githubusercontent.com/willoutcault/608_final/master/us_cities.csv', TRUE, ",")
 
-ui <- fluidPage(theme = shinytheme("superhero"),
-    
- #   shinythemes::themeSelector(),
-    navbarPage(
-        title="Job Board",
-        id="nav",
-        sidebarLayout(
-            sidebarPanel(
-                textInput("job", "Job Title: ", value="", placeholder="Job Title, Skill, Industry"),
-                tags$hr(),
-                textInput("location", "Location: ", value="", placeholder="City, State (i.e. 'Albany, NY')"),
-                actionButton("go", "Search")
-            ),
-            mainPanel(
-                leafletOutput("map"),
-                hr(),
-                fluidRow(
-                    column(4,
-                           h4("Skills"),
-                           plotOutput("plot1"),
-                    ),
-                    column(4,
-                           h4("Wage"),
-                           plotOutput("plot2"),
-                    ),
-                    column(4,
-                           h4("Location"),
-                           plotOutput("plot3"),
-                    )
-                )
-                
-            )             
+header <- dashboardHeader(
+    title = "Job Board"
+)
+
+body <- dashboardBody(
+    fluidRow(
+        column(width = 9,
+               box(width = NULL, solidHeader = TRUE,
+                   leafletOutput("map", height = 500)
+               ),
+               box(width = NULL,
+                   DT::dataTableOutput("jobPositionsTable"),
+               )
+        ),
+        column(width = 3,
+               box(width = NULL, status = "warning",
+                   textInput("job", label= NULL, value="", placeholder="Job Title, Skill, Industry"),
+                   
+                   p(
+                       class = "text-muted",
+                       paste("Ex. Data Scientist"
+                       )
+                   ),
+                   textInput("location", label= NULL, value="", placeholder="City, State"),
+                   
+                   p(
+                       class = "text-muted",
+                       paste("Ex. Albany, NY"
+                       )
+                   ),
+                   actionButton("go", "Search"),
+               ),
+               box(width = NULL, status = "warning",
+                   plotOutput("plot1"),
+                   plotOutput("plot2"),
+                   plotOutput("plot3"),
+                   p(class = "text-muted"
+                   )
+               )
         )
     )
 )
 
-
-
+ui <- dashboardPage(
+    header,
+    dashboardSidebar(disable = TRUE),
+    body
+)
 
 server <- function(input,output){
     
     
     #skills and details
-
+    
     jobskills <- eventReactive(input$go, {
         skillscrape(input$job, input$location)
     })
-
+    
     
     jobdetails <- eventReactive(input$go,{
         detailscrape(input$job, input$location)
     })
-
     
     
     
-    ## Plot1
-    output$plot2<- renderPlot({
-        ggplot(count_wages(jobdetails()), aes(x=salary))+
-            geom_density(color="darkblue", fill="lightblue")+
-            geom_vline(aes(xintercept=mean(salary)),color="blue", linetype="dashed", size=1)+
-            xlab("Salary (thousands)")+
-            theme(axis.title.y=element_blank(),
-                  axis.text.y=element_blank(),
-                  axis.ticks.y=element_blank())})
-    ## Plot2
-    output$plot3<- renderPlot({
-        ggplot(count_location(jobdetails()), aes(x=reorder(Location, n),y=n, label="count")) +
-            geom_bar(stat='identity', width=.5, fill = "lightblue", color = "darkblue")  +
-            scale_fill_manual(name="Cities Hiring") + 
-            coord_flip()+
-            xlab("City")+
-            theme(axis.title.x=element_blank(),
-                  axis.text.x=element_blank(),
-                  axis.ticks.x=element_blank())})
-    ## Map
-    output$map <- renderLeaflet({
-        coordsdf <- count_location(jobdetails())
-        coordsdf$lat <- get_lat(coords,coordsdf)
-        coordsdf$lng <- get_lng(coords,coordsdf)
-        leaflet(coordsdf) %>%
-            addTiles() %>%
-            addCircleMarkers(lat = ~lat, lng = ~lng)
-    })
-    #Plot3
     output$plot1<- renderPlot({
-        ggplot(count_skills(jobskills()), aes(x=reorder(skills, n),y=n, label="count")) +
+        
+        ggplot(count_skills(jobskills())[1:10,], aes(x=reorder(skills, n),y=n, label="count")) +
             geom_bar(stat='identity', width=.5, fill = "lightblue", color = "darkblue") +
             scale_fill_manual(name="Data Analyst Skills") + 
             coord_flip()+
@@ -102,10 +87,68 @@ server <- function(input,output){
                   axis.text.x=element_blank(),
                   axis.ticks.x=element_blank())+
             xlab("Skill")
-        })
+        
+    })
+
+    output$plot2<- renderPlot({
+        
+        ggplot(count_wages(jobdetails()), aes(x=salary))+
+            geom_density(color="darkblue", fill="lightblue")+
+            geom_vline(aes(xintercept=mean(salary)),color="blue", linetype="dashed", size=1)+
+            xlab("Salary (thousands)")+
+            theme(axis.title.y=element_blank(),
+                  axis.text.y=element_blank(),
+                  axis.ticks.y=element_blank())
+        
+    })
+
+    output$plot3<- renderPlot({
+        
+        ggplot(count_location(jobdetails())[1:10,], aes(x=reorder(Location, n),y=n, label="count")) +
+            geom_bar(stat='identity', width=.5, fill = "lightblue", color = "darkblue")  +
+            scale_fill_manual(name="Cities Hiring") + 
+            coord_flip()+
+            xlab("City")+
+            theme(axis.title.x=element_blank(),
+                  axis.text.x=element_blank(),
+                  axis.ticks.x=element_blank())
+        
+    })
+
+    output$map <- renderLeaflet({
+        coordsdf <- count_location(jobdetails())
+        coordsdf$lat <- get_lat(coords,coordsdf)
+        coordsdf$lng <- get_lng(coords,coordsdf)
+        leaflet(coordsdf) %>%
+            addTiles() %>%
+            addCircles(lat = ~lat,
+                             lng = ~lng,
+                             weight = 1,
+                             popup = paste("<b>Job Count: </b>",coordsdf$n,"<br>","<b>City: </b>",coordsdf$Location),
+                             radius = ~ sqrt(n)*1000)
+        
+    })
+
+    output$jobPositionsTable <- renderDataTable({
+        dfdet <- jobdetails()
+        dfskill <- jobskills()
+        merged_df <- dfdet
+        merged_df$Skills <- dfskill$Skills[cbind(
+            match(merged_df$urls, dfskill$urls))]
+        positions <- select(merged_df, Title, Location, Salary, Skills)
+        positions <- cbind("(index)" = 1:nrow(positions), positions)
+        if (nrow(positions) == 0)
+            return(NULL)
+        
+        datatable(positions, options = list(paging = FALSE, scrollY = "225px"), rownames = FALSE)
+    })
+
+
     
+    
+    
+    closeAllConnections()
 }
 
 
 shinyApp(ui = ui, server = server)
-
