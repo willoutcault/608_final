f3aa4d5ff430f256d8fa74c5377a0a71095eaccc
willoutcault <57675974+willoutcault@users.noreply.github.com>
2022-08-06 13:31:15 -0400

    Update scrapingtools.R

--- scrapingtools.R
+++ scrapingtools.R
@@ -188,10 +188,7 @@ leaflet_points <- function(df,df2, coords){
   
   locations_df <- df %>% 
     group_by(Location) %>% 
-    summarise(Job_Count = n(), Salary = mean(Salary)) %>% 
-    separate(Location, into=c("State","City"), sep = " - ")
-  
-  locations_df$Location <- paste(locations_df$City,locations_df$State,sep=", ")
+    summarise(Job_Count = n(), Salary = mean(Salary))
   
   locations_df$Skills <- skills_df$`list(Skills)`[cbind(
     match(locations_df$Location, skills_df$Location))]
